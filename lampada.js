b9724f3fc2b723cd6f22be33b494b21a537f08e3
“jogpe” <joaopedroms0203@gmail.com>
2022-12-02 15:35:53 -0300

    complete

--- lampada.js
+++ lampada.js
@@ -2,18 +2,32 @@ const turnOn = document.getElementById ( 'turnOn' );
 const turnOff = document.getElementById ( 'turnOff' );
 const lamp = document.getElementById ( 'lamp' );
 
-function lampOn (){
 
-    lamp.src = './images/ligada.jpg';
-
+function isLampBroken () {
+    return lamp.src.indexOf ( 'quebrada' ) > -1
 }
 
-turnOn.addEventListener ( 'click', lampOn );
+function lampOn (){
+    if ( !isLampBroken () ) {
+
+        lamp.src = './images/ligada.jpg';
+    }
+}
 
 function lampOff (){
+    if ( !isLampBroken () ) {
 
-    lamp.src = './images/desligada.jpg';
-
+        lamp.src = './images/desligada.jpg';
+     }
 }
 
+function lampBroken() {
+    lamp.src = './images/quebrada.jpg'
+}
+
+
+turnOn.addEventListener ( 'click', lampOn );
 turnOff.addEventListener ( 'click', lampOff );
+lamp.addEventListener ('mouseover', lampOn)
+lamp.addEventListener ('mouseleave', lampOff)
+lamp.addEventListener('dblclick', lampBroken)
